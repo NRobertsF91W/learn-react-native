fe2ff5c459b74ae9b7b4e30513ecb134c938a0cc
NRobertsF91W <nathanroberts2597@gmail.com>
2021-01-31 09:36:26 +0000

    following net ninja tutorial #6

--- App.js
+++ App.js
@@ -1,11 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, TextInput} from 'react-native';
 
 export default function App() {
+  const [name, setName] = useState('Nathan')
+  const [age, setAge] = useState('23')
+
+
   return (
     <View style={styles.container}>
-      <Text>Can i make a change</Text>
+      <Text> Enter name:</Text>
+      <TextInput
+        multiline
+        style={styles.input}
+        placeholder = 'e.g. John Doe'
+        onChangeText = {(val) => setName(val)}/>
+      <TextInput
+       style={styles.input}
+       placeholder = 'e.g. 20'
+       keyboardType = 'numeric'
+       onChangeText = {(val) => setAge(val)}/>
+      <Text> Their name is {name} and their age is {age}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +29,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'green',
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input: {
+    borderWidth: 1,
+    borderColor: '#777',
+    padding: 8,
+    margin: 10,
+    width:300,
+  }
 });
