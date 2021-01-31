565181d377464d5682e8575860bb25997580bc9a
NRobertsF91W <nathanroberts2597@gmail.com>
2021-01-31 12:06:49 +0000

    Working through todo app vids

---
+++ components/deedsitem.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { StyleSheet, Text, Touchable, TouchableOpacity} from 'react-native';
+
+export default function DeedsItem({ item, pressHandler }) {
+
+    return (
+        <TouchableOpacity onPress={ () => pressHandler(item.key)}>
+            <Text style={styles.item}>{item.text}</Text>
+        </TouchableOpacity>
+    )
+}
+
+const styles = StyleSheet.create({
+    item: {
+        padding: 16,
+        marginTop: 16,
+        borderColor: '#bbb',
+        borderWidth: 1,
+        borderStyle: 'dashed',
+        borderRadius: 10
+    }
+})
