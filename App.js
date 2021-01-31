3e28d7a713b7e74db8a5e9d3ff3ad1cc0a3f7a50
NRobertsF91W <nathanroberts2597@gmail.com>
2021-01-31 12:07:14 +0000

    more updates - following yt vid

--- App.js
+++ App.js
@@ -1,43 +1,68 @@
 import { StatusBar } from 'expo-status-bar';
 import React, {useState} from 'react';
-import { StyleSheet, Text, View, TextInput} from 'react-native';
+import { StyleSheet, Text, View, TextInput, ScrollView, FlatList, Alert, TouchableWithoutFeedback, Keyboard, Touchable } from 'react-native';
+import Header from './components/header';
+import DeedsItem from './components/deedsitem';
+import AddDeed from './components/adddeed';
 
 export default function App() {
-  const [name, setName] = useState('Nathan')
-  const [age, setAge] = useState('23')
+  const [deeds, setDeeds] = useState([
+    {text:'This is where your most recent deeds appear', key:'1'},
+    {text:'More deeds', key:'2'}
+  ]);
 
+  const pressHandler = (key) => {
+    setDeeds((prevDeeds) => {
+      return prevDeeds.filter(deed => deed.key != key);
+    });
+  }
+
+  const submitHandler = (text) => {
+    if(text.length > 3) {
+      setDeeds((prevDeeds) => {
+        return [
+          {text: text, key: Math.random().toString() },
+          ...prevDeeds
+        ];
+    });
+    } else {
+      Alert.alert('OOPS!', 'Deeds must be longer than 3 characters', [
+        {text:'Understood', onPress: () => console.log('alert closed')}
+      ]);
+    }
+  }
 
   return (
-    <View style={styles.container}>
-      <Text> Enter name:</Text>
-      <TextInput
-        multiline
-        style={styles.input}
-        placeholder = 'e.g. John Doe'
-        onChangeText = {(val) => setName(val)}/>
-      <TextInput
-       style={styles.input}
-       placeholder = 'e.g. 20'
-       keyboardType = 'numeric'
-       onChangeText = {(val) => setAge(val)}/>
-      <Text> Their name is {name} and their age is {age}</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <TouchableWithoutFeedback onPress={() => {
+      Keyboard.dismiss();
+    }}>
+      <View style={styles.container}>
+          <Header />
+          <View style={styles.content}>
+            <AddDeed submitHandler={submitHandler}/>
+            <View style={styles.list}>
+              <FlatList
+                data={deeds}
+                renderItem={( {item} ) => (
+                  <DeedsItem item={item} pressHandler={pressHandler}/>
+                )}
+              />
+            </View>
+          </View>
+      </View>
+    </TouchableWithoutFeedback>
+    );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
-  input: {
-    borderWidth: 1,
-    borderColor: '#777',
-    padding: 8,
-    margin: 10,
-    width:300,
+  content: {
+    padding: 40,
+  },
+  list: {
+    marginTop: 20,
   }
 });
